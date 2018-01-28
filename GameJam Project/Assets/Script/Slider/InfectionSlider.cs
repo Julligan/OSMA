797130f8543c87f13eedb8e3c51b2e1d2e67acfc
Julligan <35269222+Julligan@users.noreply.github.com>
2018-01-28 00:01:55 -0500

    Loading Bar!!!
    Loading Bar!!!

---
+++ GameJam Project/Assets/Script/Slider/InfectionSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfectionSlider : MonoBehaviour {
+
+    public Slider MainSlider;
+
+    float slideIncrease = .001f;
+
+	void Start () {
+        MainSlider.value = .05f;
+        MainSlider.maxValue = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MainSlider.value = MainSlider.value + slideIncrease;
+	}
+}
